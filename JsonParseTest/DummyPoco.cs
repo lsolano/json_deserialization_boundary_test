af2067a7391f166a6e97e4f1d1ec3842ad280c11
Lorenzo Solano Martínez <lorenzo.sm@gmail.com>
2015-05-30 00:55:50 -0400

    Initial commit.

---
+++ JsonParseTest/DummyPoco.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace JsonParseTest
+{
+    [DataContract]
+    public class DummyPoco
+    {
+        private ISet<int> internalSet01;
+        private ISet<int> internalSet02;
+
+        public DummyPoco()
+        {
+            internalSet01 = new HashSet<int>();
+            internalSet02 = new HashSet<int>();
+        }
+
+        [DataMember]
+        [JsonProperty("fakeList01", ObjectCreationHandling = ObjectCreationHandling.Replace)]
+        public List<int> FakeList01
+        {
+            get
+            {
+                return this.internalSet01.ToList();
+            }
+            set
+            {
+                var safeList = (value ?? new List<int>());
+                this.internalSet01 = new HashSet<int>(safeList);
+            }
+        }
+
+        [DataMember]
+        [JsonProperty("fakeList02")]
+        public List<int> FakeList02
+        {
+            get
+            {
+                return this.internalSet02.ToList();
+            }
+            set
+            {
+                var safeList = (value ?? new List<int>());
+                this.internalSet02 = new HashSet<int>(safeList);
+            }
+        }
+
+        [DataMember]
+        [JsonProperty("realList")]
+        public List<int> RealList { get; set; }
+    }
+}
---
+++ JsonParseTest/JsonParseTest.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Linq;
+
+namespace JsonParseTest
+{
+    [TestFixture]
+    public class JsonDeserializeTestWithObjectCreationHandling
+    {
+        [Test]
+        public void Test_Copy_Getter_And_ObjectCreationHandling_Replace()
+        {
+            var json = @" { ""fakeList01"": [ 1, 1, 2, 2, 3, 3 ] } ";
+            var poco = JsonConvert.DeserializeObject<DummyPoco>(json);
+
+            var arr = new int[] { 1, 2, 3 };
+            Assert.AreEqual(3, poco.FakeList01.Count);
+            Assert.True(poco.FakeList01.All(elem => arr.Contains(elem)));
+        }
+
+        [Test]
+        public void Test_Copy_Getter_And_ObjectCreationHandling_Auto()
+        {
+            var json = @" { ""fakeList02"": [ 1, 1, 2, 2, 3, 3 ] } ";
+            var poco = JsonConvert.DeserializeObject<DummyPoco>(json);
+
+            Assert.AreEqual(0, poco.FakeList02.Count);
+        }
+
+        [Test]
+        public void Test_Default_Getter_And_ObjectCreationHandling_Auto()
+        {
+            var json = @" { ""realList"": [ 1, 1, 2, 2, 3, 3 ] } ";
+            var poco = JsonConvert.DeserializeObject<DummyPoco>(json);
+
+            var arr = new int[] { 1, 2, 3 };
+            Assert.AreEqual(6, poco.RealList.Count);
+            Assert.True(poco.RealList.All(elem => arr.Contains(elem)));
+        }
+
+        [Test]
+        public void Test_Copy_Getter_Not_Present_In_Json_And_ObjectCreationHandling_Replace()
+        {
+            var json = @" { ""realList"": [ 1, 1, 2, 2, 3, 3 ] } ";
+            var poco = JsonConvert.DeserializeObject<DummyPoco>(json);
+
+            Assert.IsNotNull(poco.FakeList01);
+            Assert.AreEqual(0, poco.FakeList01.Count);
+        }
+
+        [Test]
+        public void Test_Copy_Getter_Not_Present_In_Json_And_ObjectCreationHandling_Auto()
+        {
+            var json = @" { ""realList"": [ 1, 1, 2, 2, 3, 3 ] } ";
+            var poco = JsonConvert.DeserializeObject<DummyPoco>(json);
+
+            Assert.IsNotNull(poco.FakeList02);
+            Assert.AreEqual(0, poco.FakeList02.Count);
+        }
+
+        [Test]
+        public void Test_Default_Getter_Not_Present_In_Json()
+        {
+            var json = @" { ""fakeList01"": [ 1, 1, 2, 2, 3, 3 ] } ";
+            var poco = JsonConvert.DeserializeObject<DummyPoco>(json);
+
+            Assert.IsNull(poco.RealList);
+        }
+
+        [Test]
+        public void Test_Deserialize_All()
+        {
+            var json = @" { ""fakeList01"": [ 1, 1, 2, 2, 3, 3 ], ""realList"": [ 1, 1, 2, 2, 3, 3 ], ""fakeList02"": [ 1, 1, 2, 2, 3, 3 ] } ";
+            var poco = JsonConvert.DeserializeObject<DummyPoco>(json);
+
+            var arr = new int[] { 1, 2, 3 };
+            Assert.AreEqual(6, poco.RealList.Count);
+            Assert.True(poco.RealList.All(elem => arr.Contains(elem)));
+
+            Assert.AreEqual(3, poco.FakeList01.Count);
+            Assert.True(poco.FakeList01.All(elem => arr.Contains(elem)));
+
+            Assert.AreEqual(0, poco.FakeList02.Count);
+            Assert.True(poco.FakeList02.All(elem => arr.Contains(elem)));
+        }
+    }
+}
